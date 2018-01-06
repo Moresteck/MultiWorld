b2c8a6956889c69d066773be46f1a16ca3b12a2f
Piotr (DaAmazingShit) <da.amazing.shit@interia.pl>
2018-01-06 11:19:15 +0100

    PVP fix, and MobArena compatibility

--- src/pl/daamazingshit/mw/listeners/Entities.java
+++ src/pl/daamazingshit/mw/listeners/Entities.java
@@ -4,8 +4,13 @@ import org.bukkit.World;
 import org.bukkit.entity.Animals;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Monster;
+import org.bukkit.entity.Player;
 import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityListener;
+
+import com.garbagemule.MobArena.ArenaManager;
 
 import pl.daamazingshit.mw.managers.ConfigWorld;
 import pl.daamazingshit.mw.util.PropertyType;
@@ -61,5 +66,28 @@ public class Entities extends EntityListener {
 		case GHAST:
 			
 		}*/
+	}
+
+	@Override public void onEntityDamage(EntityDamageEvent e) {
+		if (!(e instanceof EntityDamageByEntityEvent)) {
+			return;
+		}
+		EntityDamageByEntityEvent event = (EntityDamageByEntityEvent) e;
+		Entity entityDamager = event.getDamager();
+		Entity entityDamaged = event.getEntity();
+		if ((entityDamager instanceof Player) && (entityDamaged instanceof Player)) {
+			Player damager = (Player)entityDamager;
+			Player damaged = (Player)entityDamaged;
+			if (!ConfigWorld.getAllow(PropertyType.PVP, damager.getWorld().getName())) {
+				if (ArenaManager.playerSet.contains(damager) && ArenaManager.playerSet.contains(damaged)) {
+					return;
+				}
+				e.setCancelled(true);
+				e.setDamage(0);
+			}
+		}
+		else {
+			return;
+		}
 	}
 }
