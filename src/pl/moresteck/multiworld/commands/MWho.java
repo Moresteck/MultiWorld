d4bdf641e9ef3d85b8ca7631f0f9b25df3c5907c
Piotr (DaAmazingShit) <da.amazing.shit@interia.pl>
2018-12-16 20:50:14 +0100

    Fixed anothe stupid mistake

--- src/pl/moresteck/multiworld/commands/MWho.java
+++ src/pl/moresteck/multiworld/commands/MWho.java
@@ -1,6 +1,10 @@
 package pl.moresteck.multiworld.commands;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.bukkit.ChatColor;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -48,11 +52,9 @@ public class MWho extends MCommand {
 				return;
 			}
 		}
-		this.send("Players in world \"" + MList.getColor(world) + world.getName() + ChatColor.WHITE + "\" (" + MultiWorld.server.getOnlinePlayers().length + "):");
-		for (Player p : MultiWorld.server.getOnlinePlayers()) {
-			if (p.getWorld().getName().equals(world.getName())) {
-				this.send(" - " + ChatColor.YELLOW + p.getName() + ChatColor.WHITE + (p.getDisplayName().equals(p.getName()) ? ", nick: " + ChatColor.YELLOW + p.getDisplayName() : ""));
-			}
+		this.send("Players in world \"" + MList.getColor(world) + world.getName() + ChatColor.WHITE + "\" (" + Online.getPlayers(world.getWorld()).size() + "):");
+		for (String p : Online.getPlayers(world.getWorld())) {
+			this.send(" - " + ChatColor.YELLOW + p);
 		}
 	}
 
@@ -63,5 +65,17 @@ public class MWho extends MCommand {
 		this.send(" ");
 		this.send(ChatColor.DARK_AQUA + "Permission: " + (this.hasPermission() ? ChatColor.GREEN : ChatColor.RED) + this.perm);
 		this.send(ChatColor.DARK_AQUA + "Info: " + ChatColor.WHITE + "Shows who is currently playing in the specified world");
+	}
+
+	static class Online {
+		public static List<String> getPlayers(World world) {
+			List<String> players = new LinkedList<String>();
+			for (Player p : MultiWorld.server.getOnlinePlayers()) {
+				if (p.getWorld().getName().equals(world.getName())) {
+					players.add(p.getName());
+				}
+			}
+			return players;
+		}
 	}
 }
